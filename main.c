a367e80cdaa495a30808067ec21b442d2a26116f
Erik Nyberg <carl.erik.nyberg@gmail.com>
2026-01-05 15:23:40 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void weatherMenu(){
+    printf("+---------------------+\n");
+    printf("|       INNOMHUS      |\n");
+    printf("+---------------------+\n");
+    printf("|   Temperatur:       |\n");
+    printf("|                     |\n");
+    printf("|   Luftfuktighet:    |\n");
+    printf("+---------------------+\n");
+    printf("+---------------------+\n");
+    printf("|       UTOMHUS       |\n");
+    printf("+---------------------+\n");
+    printf("|   Temperatur:       |\n");
+    printf("|                     |\n");
+    printf("|   Luftfuktighet:    |\n");
+    printf("+---------------------+\n");
+}
+void outsideMenu(){
+
+}
+
+int main (){
+    weatherMenu();
+    
+    return 0;
+}
